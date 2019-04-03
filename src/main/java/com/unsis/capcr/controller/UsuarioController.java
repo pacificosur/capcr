566e1e6b56ca41736e59b9d8492321094a07a7fa
labtw04 <labtw04@labtw04-OptiPlex-9020>
2019-04-02 18:44:30 -0600

    Modificacion Hiralda

--- src/main/java/com/unsis/capcr/controller/UsuarioController.java
+++ src/main/java/com/unsis/capcr/controller/UsuarioController.java
@@ -11,6 +11,6 @@ public class UsuarioController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        int x;
     }  
 }
