ac882d9ce0779bf4011982d5ea994aa420b49596
LabTW12 <LabTW12@LabTW12-PC>
2019-04-06 00:11:10 -0600

    Resolver errores

--- src/main/java/com/unsis/capcr/db/Connection.java
+++ src/main/java/com/unsis/capcr/db/Connection.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**/
 package com.unsis.capcr.db;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author LabTW01
- */
+
 public class Connection {
 private final String url= "jdbc:postgresql://localhost/Usuario";
 	private final String user= "postgres";
@@ -20,7 +13,7 @@ private final String url= "jdbc:postgresql://localhost/Usuario";
 	public Connection conecta() {
 		Connection conn= null;
 		try {
-			conn= DriverManager.getConnection(url, user, password);
+			conn= (Connection) DriverManager.getConnection(url, user, password);
 			System.out.println("Conexión Establecida");
 		}
 		catch(SQLException e) {
