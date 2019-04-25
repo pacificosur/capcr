f0a3a7c1c2eb6f4c8ccf66c55529d61b6d40b9d1
LabTW03 <LabTW03@LabTW03-PC>
2019-04-24 22:36:53 -0500

    Modificación de jsp home de l carpeta inicio

--- src/main/java/com/unsis/capcr/controller/InicioController.java
+++ src/main/java/com/unsis/capcr/controller/InicioController.java
@@ -5,8 +5,13 @@
  */
 package com.unsis.capcr.controller;
 
+import com.unsis.capcr.entity.Reservacion;
+import com.unsis.capcr.service.IReservacionService;
+import com.unsis.capcr.service.ReservacionService;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,50 +22,14 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author labso04
  */
-@WebServlet(name = "Inicio", urlPatterns = {"/Inicio"})
+
 public class InicioController extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Inicio</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Inicio at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String accion = request.getParameter("accion");    
     }
-
+    
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -72,17 +41,19 @@ public class InicioController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        System.out.println("hola post");
+        doGet(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    
+    private void home(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/inicio/home.jsp");
+        dispatcher.forward(request, response);
+    }
+  
+    private void listar(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/inicio/listar.jsp");
+	dispatcher.forward(request, response);        
+    }
 }
