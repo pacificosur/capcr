0f8f63131a33609bd5d453eeefaed06609300059
LabTW03 <LabTW03@LabTW03-PC>
2019-05-29 23:29:12 -0500

    Eliminación de código que no se ocupa

--- src/main/java/com/unsis/capcr/controller/LogueoController.java
+++ src/main/java/com/unsis/capcr/controller/LogueoController.java
@@ -5,7 +5,7 @@
   Fecha Modificación: 03/05/2019
   Descripción: Servlet que comprueba si el logueo es correcto
 Tecnologias web I
-*/
+ */
 package com.unsis.capcr.controller;
 
 import com.unsis.capcr.entity.Usuario;
@@ -21,32 +21,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-
 public class LogueoController extends HttpServlet {
+
     @Override
-   
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            IUsuarioService iuser=new UsuarioService();
-            response.setContentType("Text/html");
-            PrintWriter salida= response.getWriter(); 
-            IUsuarioModel iu= new UsuarioModel();
-            Usuario usr=new Usuario();
-            String user=request.getParameter("user");
-            String password=request.getParameter("password");   
-            usr.setNombreUsuario(user);
-            usr.setContraseña(password);
-            if(iuser.logueo(usr)){
-                HttpSession nueva_sesion = request.getSession(true);
-                nueva_sesion.setAttribute("user", user);
-                response.sendRedirect("/capcr/pages/index.jsp");/*Manda a la página inicial del proyecto*/
-            }
-            else{
-                response.sendRedirect("/capcr/pages/logueo/index.jsp");/*Contraseña incorrecta vuelve a recargar la página*/
-            }
-            
+        IUsuarioService iuser = new UsuarioService();
+        response.setContentType("Text/html");
+        PrintWriter salida = response.getWriter();
+        IUsuarioModel iu = new UsuarioModel();
+        Usuario usr = new Usuario();
+        String user = request.getParameter("user");
+        String password = request.getParameter("password");
+        usr.setNombreUsuario(user);
+        usr.setContraseña(password);
+        if (iuser.logueo(usr)) {
+            HttpSession nueva_sesion = request.getSession(true);
+            nueva_sesion.setAttribute("user", user);
+            response.sendRedirect("/capcr/pages/index.jsp");/*Manda a la página inicial del proyecto*/
+        } else {
+            response.sendRedirect("/capcr/pages/logueo/index.jsp");/*Contraseña incorrecta vuelve a recargar la página*/
+        }
+
     }
-    
 
 }
