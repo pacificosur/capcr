7214ce70eeb373591b4dbfb1023b7d96d2cd5bd7
Uriel <padilla98.upb@gmail.com>
2019-06-20 19:00:00 -0500

    Se creo el servlet ReporteController Se modificó el Reporte Jasper

--- src/main/java/com/unsis/capcr/controller/ReporteController.java
+++ src/main/java/com/unsis/capcr/controller/ReporteController.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Uriel
  */
-public class ReporteControler extends HttpServlet {
+public class ReporteController extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -35,10 +35,10 @@ public class ReporteControler extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ReporteControler</title>");            
+            out.println("<title>Servlet ReporteController</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ReporteControler at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet ReporteController at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
